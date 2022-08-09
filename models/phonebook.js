ea2e2091c1a4164bbc621a9af9889c1f8d6a6b18
Anthony P <43166263+anthonypz@users.noreply.github.com>
2022-08-09 15:25:32 -0700

    Add mongoose validation for the person schema

--- models/phonebook.js
+++ models/phonebook.js
@@ -3,8 +3,26 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'A person name is required'],
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: [true, 'A person phone number is required'],
+    validate: {
+      validator: function (v) {
+        return (
+          /\d{3}-\d{3}-\d{4}/.test(v) ||
+          /\d{2,3}-\d+/.test(v) ||
+          /\d{1}-\d{3}-\d{3}-\d{4}/.test(v)
+        );
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set('toJSON', {
